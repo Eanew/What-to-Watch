3264a558764f3577bed16d4c2e0b95b6d5cc094e
Eanew <H.Eanew@yandex.ru>
2020-07-30 12:43:39 +0300

    Микроисправления

--- src/components/app/app.test.js
+++ src/components/app/app.test.js
@@ -2,13 +2,14 @@ import React from "react";
 import renderer from "react-test-renderer";
 import App from "./app.jsx";
 
-import {promo, films} from "../../mocks/test-mock.js";
+import {promo, films, reviews} from "../../mocks/test-mock.js";
 
 it(`Should AppComponent render correctly`, () => {
   const tree = renderer.create(
       <App
         promo={promo}
         films={films}
+        reviews={reviews}
         renderApp={() => {}}
         onFilmCardClick={() => {}}
       />
